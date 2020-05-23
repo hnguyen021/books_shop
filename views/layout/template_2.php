93c75b31f59cf47f4e1141718fae8668fb9ff2ad
mrbossgame <55154165+mrbossgame@users.noreply.github.com>
2020-05-24 00:35:40 +0700

    Update template_2.php
    aaa

--- views/layout/template_2.php
+++ views/layout/template_2.php
@@ -6,31 +6,16 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 -->
 <!DOCTYPE HTML>
 <head>
-<title>Free Home Shoppe Website Template | Preview :: w3layouts</title>
+<title>Book Shop</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 <link href="http://localhost:8888/MVC/assets/css/style.css" rel="stylesheet" type="text/css" media="all"/>
+<link href="http://localhost:8888/MVC/assets/css/slider.css" rel="stylesheet" type="text/css" media="all"/>
 <script type="text/javascript" src="http://localhost:8888/MVC/assets/js/jquery-1.7.2.min.js"></script> 
 <script type="text/javascript" src="http://localhost:8888/MVC/assets/js/move-top.js"></script>
 <script type="text/javascript" src="http://localhost:8888/MVC/assets/js/easing.js"></script>
-<script src="http://localhost:8888/MVC/assets/js/easyResponsiveTabs.js" type="text/javascript"></script>
-<link href="http://localhost:8888/MVC/assets/css/easy-responsive-tabs.css" rel="stylesheet" type="text/css" media="all"/>
-<link rel="stylesheet" href="http://localhost:8888/MVC/assets/css/global.css">
-<script src="http://localhost:8888/MVC/assets/js/slides.min.jquery.js"></script>
-<script>
-		$(function(){
-			$('#products').slides({
-				preload: true,
-				preloadImage: 'img/loading.gif',
-				effect: 'slide, fade',
-				crossfade: true,
-				slideSpeed: 350,
-				fadeSpeed: 500,
-				generateNextPrev: true,
-				generatePagination: false
-			});
-		});
-	</script>
+<script type="text/javascript" src="http://localhost:8888/MVC/assets/js/startstop-slider.js"></script>
 </head>
 <body>
   <div class="wrap">
@@ -54,6 +39,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 					<?php
 						}
 					?>
+					
 					<li><a href="#">Delivery</a></li>
 					<li><a href="#">Checkout</a></li>
 					<li><a href="#">My Account</a></li>
@@ -65,12 +51,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 			<div class="logo">
 				<a href="index.html"><img src="images/logo.png" alt="" /></a>
 			</div>
-			  <div class="cart">
-			  	   <p>Welcome to our Online Store! <span>Cart:</span><div id="dd" class="wrapper-dropdown-2"> 0 item(s) - $0.00
-			  	   	<ul class="dropdown">
-							<li>you have no items in your Shopping cart</li>
-					</ul></div></p>
-			  </div>
+			  
 			  <script type="text/javascript">
 			function DropDown(el) {
 				this.dd = el;
@@ -102,18 +83,17 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 	 <div class="clear"></div>
   </div>
 	<div class="header_bottom">
-	<div class="menu">
+	     	<div class="menu">
 	     		<ul>
-			    	<li class="active"><a href="index.html">Home</a></li>
+			    	<li class="active"><a href="?controller=home&action=index">Home</a></li>
 			    	<li><a href="about.html">About</a></li>
 			    	<li><a href="delivery.html">Delivery</a></li>
 			    	<li><a href="news.html">News</a></li>
-			    	<li><a href="contact.html">Contact</a></li>
-					
+					<li><a href="contact.html">Contact</a></li>
 					<?php
 						if(isLoggedIn()){
 					?>
-						<li><a href="contact.html">Your Cart</a></li>
+						<li><a href="?controller=cart">Your Cart</a></li>
 						<li><a href="?controller=cart&action=bill">Your Bill</a></li>
 					<?php
 					}
@@ -127,42 +107,44 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 	     		</form>
 	     	</div>
 	     	<div class="clear"></div>
-	     </div>	     	
-   </div>
- <div class="main">
-    <div class="content">
-    	<div class="content_top">
-    		<div class="back-links">
-    		<a href="?controller=home&action=index">
-    	    </div>
-    		<div class="clear"></div>
-    	</div>
-    	<div class="section group">
-                <?= $content_main?> 
-				<div class="rightsidebar span_3_of_1">
-					<h2>CATEGORIES</h2>
-					<div class="categories">
+	     </div>	     
+	<div class="header_slide">
+			<div class="header_bottom_left">				
+				<div class="categories">
 				  <ul>
-				  
+				  	<h3>Categories</h3>
 					  <?php
-					  	$i = 1;
 					 	foreach($categories as $c){
 							 ?>
 							<li><a href="?controller=book&action=category&id=<?=$i?>"><?= $c->tentheloai?></a></li>
 							<?php
-							$i+=1;
 						 } 
 					  ?>
 				      
 				      
 				  </ul>
-				</div>
-    				
-      				 
- 		</div>
- 	</div>
-    </div>
- </div>
+				</div>					
+	  	     </div>
+					 <div class="header_bottom_right">					 
+					 	 <div class="slider">					     
+						  <!--	<=$content_slider?>-- CONTENT FOR SLIDER--> 
+					
+							<div id="slider">
+                            <?=$content_main?>
+			                    <div id="mover">
+			                    	
+									     												
+			                 </div>		
+		                </div>
+					 <div class="clear"></div>					       
+		         </div>
+		      </div>
+		   <div class="clear"></div>
+		</div>
+   </div>
+ <div class="main">
+  
+</div>
    <div class="footer">
    	  <div class="wrap">	
 	     <div class="section group">
@@ -220,7 +202,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 				<p>&copy; 2013 home_shoppe. All rights reserved | Design by <a href="http://w3layouts.com/">W3layouts</a></p>
 		   </div>
     </div>
-   <script type="text/javascript">
+    <script type="text/javascript">
 		$(document).ready(function() {			
 			$().UItoTop({ easingType: 'easeOutQuart' });
 			
